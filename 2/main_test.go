3f17b54ce82880841805810fe1de6af965436f7f
Josh Whetton <whetton.josh@gmail.com>
2024-12-05 09:16:51 -0500

    test: Add final test for inc/dec
    Check for increase and decrease in the same reading.

--- 2/main_test.go
+++ 2/main_test.go
@@ -22,10 +22,21 @@ func (st DecreaseTest) Test() bool {
 	return mustDecrease(st.Reading)
 }
 
+type SafetyTest struct {
+	Reading     []int
+	Expected    bool
+	Description string
+}
+
+func (st SafetyTest) Test() bool {
+	return isSafe(st.Reading)
+}
+
 func TestMustIncrease(t *testing.T) {
 	tests := []IncreaseTest{
 		{[]int{1, 3, 6, 7, 9}, true, "Stable increase"},
 		{[]int{1, 3, 6, 6, 7}, false, "Unstable increase; doubles"},
+		{[]int{1, 3, 6, 5, 7}, false, "Unstable increase; increase and decrease"},
 		{[]int{7, 6, 4, 2, 1}, false, "Decrease"},
 	}
 
@@ -40,7 +51,21 @@ func TestMustDecrease(t *testing.T) {
 	tests := []DecreaseTest{
 		{[]int{7, 6, 4, 2, 1}, true, "Stable decrease"},
 		{[]int{7, 6, 6, 4, 2}, false, "Unstable decrease; doubles"},
+		{[]int{7, 6, 7, 4, 2}, false, "Unstable decrease; increase and decrease"},
 		{[]int{1, 2, 7, 8, 9}, false, "Increase"},
+	}
+
+	for _, test := range tests {
+		if test.Test() != test.Expected {
+			t.Errorf("%s - input: %+v, wanted %t", test.Description, test.Reading, test.Expected)
+		}
+	}
+}
+
+func TestSafety(t *testing.T) {
+	tests := []SafetyTest{
+		{[]int{}, true, "Safe"},
+		{[]int{}, true, "Unsafe"},
 	}
 
 	for _, test := range tests {
